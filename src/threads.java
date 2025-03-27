fbb0fd2907e2e3114e457ee3306920af0c9e096c
weewewe117 <kae1yaalbericch@gmail.com>
2025-03-27 18:31:33 +0200

    Initial commit

---
+++ src/threads.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+
+public class еркуфвы {
+    public static void main(String[] args) {
+        int numberOfThreads = 10;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            Thread thread = new Thread(() -> {
+                int randomNumber = new Random().nextInt(101);
+                System.out.println("поток" + Thread.currentThread().getName() + " - число: " + randomNumber);
+            });
+            thread.start();
+        }
+
+
+
+        // вопросики с количеством потоков
+
+
+        int largeThreadCount = 100;
+        for (int i = 0; i < largeThreadCount; i++) {
+            Thread thread = new Thread(() -> {
+                int randomNumber = new Random().nextInt(100);
+                System.out.println("поток " + Thread.currentThread().getName() + " - число: " + randomNumber);
+            });
+            thread.start();
+        }
+    }
+}
